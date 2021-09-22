5258be8235cb45c2e750485b300925019401b624
Tidal-Wave <vodyannikov.eugene@gmail.com>
2021-09-22 13:16:54 +0500

    DB created

---
+++ WorldSkills/DataBase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace WorldSkills
+{
+    class DataBase
+    {
+        string connectionString;
+        SqlConnection connection;
+
+        void OpenConnection()
+        {
+            connectionString = @"Data Source=SQLDEM\SQLDEM;Initial Catalog=pr1-22-vodyannikoves_WorldSkills;"
+            + "Integrated Security=True";
+            connection = new SqlConnection(connectionString);
+            connection.Open();
+        }
+
+        void CloseConnection()
+        {
+            connection.Close();
+        }
+    }
+}
